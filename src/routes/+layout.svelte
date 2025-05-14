3918045128003a61009db19bb2e4cc41924823ae
Chris <christian.bang@gmail.com>
2025-05-14 14:25:01 +0200

    layout changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,13 @@
 	let { children } = $props();
 </script>
 
-<div class="mx-auto max-w-6xl rounded-xl border-white/60 bg-white/70 py-12">
-	{@render children()}
-</div>
+<main class="flex h-screen w-screen items-center justify-center">
+	<div
+		class=" flex items-center justify-center rounded-xl border-white/60 bg-white/70 py-12 lg:w-3xl"
+	>
+		{@render children()}
+	</div>
+</main>
 
 <svelte:head>
 	<title>LoL Guess the Splash!</title>
